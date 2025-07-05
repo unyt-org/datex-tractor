f9a6ab40aafa121c8e3b0eea82c33eb75b4d69be
janiejestemja <janiejestemja@email>
2025-07-05 14:37:37 +0200

    wrapped main.py into todo_script.sh and setup README.md section for test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from todo_context import TodoContext
 
-def main():
+def legacy_main():
     src_path = "."
     todo_paths = list(TodoContext.initialize_paths(src_path))
     todo_paths.sort(key=lambda x: x.path)
@@ -10,6 +10,23 @@ def main():
         print(path.path)
         for line_number, line in zip(path.line_numbers, path.matched_lines):
             print(f"  line {line_number}: {line}")
+
+    return 1
+
+def main():
+    lines = []
+    with open("README.md") as f:
+        reader = f.readlines()
+        lines = [line for line in reader]
+    
+    last_line = int(lines[-1].strip()) + 1
+    lines[-1] = str(last_line)
+
+
+    with open("README.md", "w") as f:
+        f.write("".join([str(line) for line in lines]))
+
+    return 0
 
 if __name__ == "__main__":
     main()
