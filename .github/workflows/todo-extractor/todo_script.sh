c89a96a5cc17749a1fe418cdb810db1579af1841
janiejestemja <janiejestemja@email>
2025-07-07 01:50:27 +0200

    added sketch/ptype as workflow

---
+++ .github/workflows/todo-extractor/todo_script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+echo "Entry point of todo extraction"
+
+if [[ "$#" != 0 ]]; then
+	echo "No CLA please."
+	exit 1
+fi
+echo "Python check..."
+python --version
+pip list
+
+echo "Running main"
+python .github/workflows/todo-extractor/main.py
+
+if [[ "$?" != 0 ]]; then
+	echo "main.py execution failed."
+	exit 1
+fi
+
+git config --global user.name "github-actions"
+git config --global user.email "github-actions@github.com"
+
+git add README.md
+
+if git diff --cached --quiet; then
+	echo "No changes to commit."
+	exit 0
+else
+	echo "Commit"
+	git commit -m "auto update readme"
+	echo "push"
+	git push
+fi
+
