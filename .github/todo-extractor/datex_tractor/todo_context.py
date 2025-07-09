605536518c344641f358b7349a5633300b619c0f
janiejestemja <janiejestemja@email>
2025-07-09 10:15:22 +0200

    added todos for trial run

--- .github/todo-extractor/datex_tractor/todo_context.py
+++ .github/todo-extractor/datex_tractor/todo_context.py
@@ -171,6 +171,8 @@ class TodoContext():
         new_comment = "Undescribed by author."
         for i, line in enumerate(lines):
 
+            # todo!()
+            # todo!("With a comment")
             if match := cls.todo_makro.search(line):
                 if not match.group("number"):
                     
@@ -193,6 +195,7 @@ class TodoContext():
                     "comment": match.group("comment").rstrip('"') if match.group("comment") else new_comment
                 })
 
+            # TODO Refactor some day, maybe...
             elif match := cls.todo_comment.search(line):
 
                 # Place issues number if not there
@@ -210,6 +213,7 @@ class TodoContext():
                     "comment": match.group("comment") if match.group("comment") else new_comment
                 })
 
+            # FIXME later, if you have the time...
             elif match := cls.fixme_comment.search(line):
                 # Place issues number if not there
                 if not match.group("number"):
