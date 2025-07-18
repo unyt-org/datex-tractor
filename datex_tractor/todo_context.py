d900508e81f993644c2d6c92c72255429b1e5f68
janiejestemja <janiejestemja@email>
2025-07-18 11:44:58 +0200

    restructured code

--- datex_tractor/todo_context.py
+++ datex_tractor/todo_context.py
@@ -13,8 +13,7 @@ class TodoContext():
     )
     todo_makro = re.compile(r'\b(todo!)\("?#?(?P<number>\d+)?"?:?(?P<comment>.*)\)(?P<tail>.*)') 
 
-
-    def __init__(self, path):
+    def __init__(self, path: str):
         self.path = path
 
         self.first_findings = []
@@ -27,117 +26,7 @@ class TodoContext():
         self.author_comments = []
 
     @classmethod
-    def scan_for_todos(cls, filepath):
-        """Minimalistic Sketch"""
-        with open(filepath, errors="ignore") as f:
-            lines = f.readlines()
-
-        findings = []
-        for i, line in enumerate(lines):
-
-            if match := cls.todo_makro.search(line):
-                findings.append({
-                    "line_number": i,
-                    "extracted_match": str(match.group()),
-                    })
-
-            elif match := cls.todo_comment.search(line):
-                findings.append({
-                    "line_number": i,
-                    "extracted_match": str(match.group()),
-                })
-
-            elif match := cls.fixme_comment.search(line):
-                findings.append({
-                    "line_number": i,
-                    "extracted_match": str(match.group()),
-                })
-
-        return findings
-
-    @classmethod
-    def initialize_paths(cls, src_path, issue_counter=11):
-        extensions = [".rs", ".cpp", ".py", ".sh",".java", ".ts", ".js", ".php"]
-        todo_paths = set()
-
-        # Crawling through current work directory
-        for root, _, files in os.walk(src_path):
-            for file in files:
-
-                # Checking python files 
-                if any([file.endswith(ext) for ext in extensions]): 
-                    path = os.path.join(root, file)
-
-                    # Checking regexes 
-                    findings, issue_counter = cls.scan_for_issues(path, issue_counter)
-                    if findings:
-                        tempTodoPath = TodoContext(path)
-
-                        # Memorize first findings
-                        for f in findings:
-                            tempTodoPath.first_findings.append(f)
-
-                        todo_paths.add(tempTodoPath)
-
-        # Separation of line numbers and descriptions of first findings
-        for path in todo_paths:
-            path.line_numbers += [x["line_number"] for x in path.first_findings]
-            path.matched_lines += [x["extracted_match"] for x in path.first_findings]
-            path.lines += [x["line"] for x in path.first_findings]
-            path.issue_numbers += [x["issue_number"] for x in path.first_findings]
-            path.author_comments += [x["comment"] for x in path.first_findings]
-
-        return todo_paths
-
-    @classmethod
-    def readme_sentinel(cls, issue_counter=11):
-        todo_list_string = cls.get_todo_listed_issues(issue_counter)
-        if todo_list_string == 1:
-            print("Creation of todo list failed, found nothing to do.")
-            todo_list_string = "Found nothing to do.\n"
-
-        lines = []
-        try:
-            with open("README.md", mode="r", encoding="utf-8") as f:
-                reader = f.readlines()
-                lines = [line for line in reader]
-        except Exception:
-            print("File named 'README.md' not found in root directory.")
-            return 1
-        
-        # Remember last line and increment it a priori
-        try:
-            last_line = int(lines[-1].strip()) + 1
-        except Exception:
-            print("Can't find number in last line of readme, using default.")
-            last_line = int(-4057)
-
-        # Match sentinel index
-        todo_sentinel_start = "# Datex-tractor"
-        matches = [(i, line) for i, line in enumerate(lines) if line.startswith(todo_sentinel_start)]
-
-        if len(matches) != 1:
-            print("Clear resolution of sentinel header failed")
-            return 1
-
-        # Unpack index
-        header_line, _ = matches[0]
-
-        # Cut off after sentinel index
-        lines = lines[:header_line + 2]
-        lines.append(todo_list_string)
-
-        # Append incremented remembered last line
-        lines.append(str(last_line))
-
-        # Overwrite old sentinel
-        with open("README.md", mode="w", encoding="utf-8") as f:
-            f.write("".join([str(line) for line in lines]))
-
-        return 0
-
-    @classmethod
-    def scan_for_issues(cls, filepath, issue_counter=11):
+    def scan_for_issues(cls, filepath: str, issue_counter: int):
 
         with open(filepath, errors="ignore") as f:
             lines = f.readlines()
@@ -205,7 +94,41 @@ class TodoContext():
         return findings, issue_counter
 
     @classmethod
-    def get_todo_listed_issues(cls, issue_counter=11):
+    def initialize_paths(cls, src_path: str, issue_counter: int):
+        extensions = [".rs", ".cpp", ".py", ".sh",".java", ".ts", ".js", ".php"]
+        todo_paths = set()
+
+        # Crawling through current work directory
+        for root, _, files in os.walk(src_path):
+            for file in files:
+
+                # Checking python files 
+                if any([file.endswith(ext) for ext in extensions]): 
+                    path = os.path.join(root, file)
+
+                    # Checking regexes 
+                    findings, issue_counter = cls.scan_for_issues(path, issue_counter)
+                    if findings:
+                        tempTodoPath = TodoContext(path)
+
+                        # Memorize first findings
+                        for f in findings:
+                            tempTodoPath.first_findings.append(f)
+
+                        todo_paths.add(tempTodoPath)
+
+        # Separation of line numbers and descriptions of first findings
+        for path in todo_paths:
+            path.line_numbers += [x["line_number"] for x in path.first_findings]
+            path.matched_lines += [x["extracted_match"] for x in path.first_findings]
+            path.lines += [x["line"] for x in path.first_findings]
+            path.issue_numbers += [x["issue_number"] for x in path.first_findings]
+            path.author_comments += [x["comment"] for x in path.first_findings]
+
+        return todo_paths
+
+    @classmethod
+    def get_todo_listed_issues(cls, issue_counter: int):
         src_path = "."
         todo_paths = list(cls.initialize_paths(src_path, issue_counter))
         todo_paths.sort(key=lambda x: x.path)
@@ -228,9 +151,58 @@ class TodoContext():
             desc += f"## '{todo_path.path.removeprefix("./")}'\n"
             for i, line_number in enumerate(todo_path.line_numbers):
                 desc += f"- Id: #{todo_path.issue_numbers[i]}\n"
-                # desc += f"- {line_number}: '{todo_path.author_comments[i]}'\n"
                 # Debug
+                # desc += f"- {line_number}: '{todo_path.author_comments[i]}'\n"
                 # desc += f"  - Edited line: '{todo_path.lines[i].strip()}'\n"
 
         desc += "\n"
         return desc
+
+    @classmethod
+    def readme_sentinel(cls, issue_counter: int):
+        todo_list_string = cls.get_todo_listed_issues(issue_counter)
+        if todo_list_string == 1:
+            print("Creation of todo list failed, found nothing to do.")
+            todo_list_string = "Found nothing to do.\n"
+
+        lines = []
+        try:
+            with open("README.md", mode="r", encoding="utf-8") as f:
+                reader = f.readlines()
+                lines = [line for line in reader]
+        except Exception:
+            print("File named 'README.md' not found in root directory.")
+            return 1
+        
+        # Remember last line and increment it a priori
+        try:
+            last_line = int(lines[-1].strip()) + 1
+        except Exception:
+            print("Can't find number in last line of readme, using default.")
+            last_line = int(-4057)
+
+        # Match sentinel index
+        todo_sentinel_start = "# Datex-tractor"
+        matches = [(i, line) for i, line in enumerate(lines) if line.startswith(todo_sentinel_start)]
+
+        if len(matches) != 1:
+            print("Clear resolution of sentinel header failed")
+            return 1
+
+        # Unpack index
+        header_line, _ = matches[0]
+
+        # Cut off after sentinel index
+        lines = lines[:header_line + 2]
+        # Debug
+        # lines.append(todo_list_string)
+
+        # Append incremented remembered last line
+        lines.append(str(last_line))
+
+        # Overwrite old sentinel
+        with open("README.md", mode="w", encoding="utf-8") as f:
+            f.write("".join([str(line) for line in lines]))
+
+        return 0
+
