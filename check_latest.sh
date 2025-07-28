b112e069501feb949e32f100afe5f17db9e83295
Jonas Strehle <jonas@strehles.info>
2025-07-28 21:08:39 +0200

    Update check_latest.sh

--- check_latest.sh
+++ check_latest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Fetching version branches (release/*)..."
-all_branches=$(git ls-remote --heads origin "release/*" | cut -d/ -f3- | sort -V)
+all_branches=$(git ls-remote --heads origin "release/*" | cut -d/ -f3- | grep -E '^release/[^/]+$' | sort -V)
 
 latest=$(echo "$all_branches" | tail -n1)
 current="${GITHUB_REF#refs/heads/}"
