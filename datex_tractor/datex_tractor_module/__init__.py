362ec9ba6f2fb185d54a6d91096a5dee69d6df6f
janiejestemja <janiejestemja@web.de>
2025-10-25 10:02:31 +0200

    add openhermes

--- datex_tractor/datex_tractor_module/__init__.py
+++ datex_tractor/datex_tractor_module/__init__.py
@@ -2,7 +2,7 @@ from .todo_context import TodoContext
 from .gethub import get_issues, get_discussions, close_issue, reopen_issue, update_issue, create_issue
 
 try:
-    from .prompt import Prompt
+    from .hermes import Prompt
 
 except Exception:
     __all__ = [
