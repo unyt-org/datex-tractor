e525345c1d8572056a362bfeaa7e9706676f97a5
janiejestemja <janiejestemja@web.de>
2025-10-25 10:02:11 +0200

    refactor llm integration

--- datex_tractor/main.py
+++ datex_tractor/main.py
@@ -2,7 +2,14 @@ import pytest
 from datex_tractor_module import TodoContext
 
 def main():
-    raise NotImplementedError
+    issue_counter = 1
+    todo_paths = list(TodoContext.extract_codeblocks("tests/samples/", issue_counter))
+    todo_paths.sort(key=lambda x: x.path)
+
+    for path in todo_paths:
+        for i, line_number in enumerate(path.line_numbers):
+            code_block = "".join(path.code_blocks[i][2])
+            print(code_block, end="")
 
 if __name__ == "__main__":
     main()
