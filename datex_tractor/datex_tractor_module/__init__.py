aa1af1bfa0a422a08acdce888ebd2d955f27d7bf
janiejestemja <janiejestemja@web.de>
2025-10-25 10:02:33 +0200

    edit imports

--- datex_tractor/datex_tractor_module/__init__.py
+++ datex_tractor/datex_tractor_module/__init__.py
@@ -1,10 +1,8 @@
 from .todo_context import TodoContext
 from .gethub import get_issues, get_discussions, close_issue, reopen_issue, update_issue, create_issue
+import sys
 
-try:
-    from .hermes import Prompt
-
-except Exception:
+if len(sys.argv) == 2 or len(sys.argv) != 3:
     __all__ = [
         "TodoContext",
         "get_discussions",
@@ -14,15 +12,43 @@ except Exception:
         "update_issue",
         "create_issue",
     ]
-
 else:
-    __all__ = [
-        "TodoContext",
-        "get_discussions",
-        "get_issues",
-        "close_issue",
-        "reopen_issue",
-        "update_issue",
-        "create_issue",
-        "Prompt",
-    ]
+    match sys.argv[2]:
+        case "hrms":
+            try:
+                from .hermes import Prompt
+
+            except Exception:
+                raise NotImplementedError("Missing dependency")
+
+            else:
+                __all__ = [
+                    "TodoContext",
+                    "get_discussions",
+                    "get_issues",
+                    "close_issue",
+                    "reopen_issue",
+                    "update_issue",
+                    "create_issue",
+                    "Prompt",
+                ]
+        case "dpsk":
+            try:
+                from .dpsk import Prompt
+
+            except Exception:
+                raise NotImplementedError("Missing dependency")
+
+            else:
+                __all__ = [
+                    "TodoContext",
+                    "get_discussions",
+                    "get_issues",
+                    "close_issue",
+                    "reopen_issue",
+                    "update_issue",
+                    "create_issue",
+                    "Prompt",
+                ]
+        case _:
+            sys.exit()
