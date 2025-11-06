118d2b140abb951a1215f040a79583e502c3c33d
janiejestemja <janiejestemja@web.de>
2025-11-06 11:54:00 +0100

    add option to remove issue ids from codebase

--- datex_tractor_script.sh
+++ datex_tractor_script.sh
@@ -7,8 +7,11 @@ python --version
 pip list
 
 echo "Running jector.py..."
-python "${GITHUB_ACTION_PATH}/datex_tractor/datex_jector.py"
-
+if [[ "$#" == 2 ]]; then
+    python "${GITHUB_ACTION_PATH}/datex_tractor/datex_jector.py" "remove"
+else
+    python "${GITHUB_ACTION_PATH}/datex_tractor/datex_jector.py"
+fi
 
 git config --global user.name "github-actions"
 git config --global user.email "github-actions@github.com"
@@ -26,8 +29,10 @@ echo "Commit: $LAST_COMMIT"
 echo "Run tractor.py..."
 if [[ "$#" == 1 ]]; then
     python "${GITHUB_ACTION_PATH}/datex_tractor/datex_tractor.py" $LAST_COMMIT $1
-else
+elif [[ "$#" != 2 ]]; then
     python "${GITHUB_ACTION_PATH}/datex_tractor/datex_tractor.py" $LAST_COMMIT
+else
+    echo "Removed ids - nothing to do..."
 fi
 
 echo "Datex-tractor End."
