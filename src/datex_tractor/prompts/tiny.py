1bf9e6d22865b9f52ef0892bfc4bc3eba4061477
janiejestemja <janiejestemja@web.de>
2025-11-19 10:35:44 +0100

    draft for tiny model

---
+++ src/datex_tractor/prompts/tiny.py
@@ -0,0 +1,69 @@
+import os
+from llama_cpp import Llama
+
+
+class Prompt():
+    def __init__(self, instruction):
+        self.system_instruction = instruction
+        self.chat = [instruction]
+
+    def from_user(self, text: str):
+        self.chat.append("<|user|>\n" + text + "</s>")
+        self.chat.append("<|assistant|>\n")
+
+    def from_assistant(self, text: str):
+        self.chat[-1] += text + "</s>"
+
+    def get_prompt(self):
+        prompt = ""
+        for line in self.chat:
+            prompt += line
+        return prompt
+
+    @staticmethod
+    def load_model():
+        # Try loading model
+        model_path = os.getenv("MODEL_PATH")
+        prompt_path = os.getenv("PROMPT_PATH")
+        try:
+            llm = Llama(
+                model_path=model_path,
+                n_ctx=2048,
+                verbose=False,
+            )
+        except Exception:
+            print("Model not found.")
+            raise FileNotFoundError("Unresolved model path")
+
+        try:
+            with open(prompt_path) as f:
+                instruction = f.read().strip()
+        except Exception:
+            print("Prompt not found.")
+            raise FileNotFoundError("Unresolved prompt path")
+
+        return llm, instruction
+
+    @staticmethod
+    def gen_advice(llm, instruction: str, code_block: str):
+        user_input = "```\n" + code_block + "\n```"
+
+        # print("Init new prompt...")
+        sysprom = Prompt(instruction)
+        sysprom.from_user(user_input)
+        prompt = sysprom.get_prompt()
+
+        # print("Generating answer...")
+        output = llm(
+
+            prompt,
+            max_tokens=1024,
+            temperature=0.48,
+            top_p=0.91,
+            top_k=50,
+            repeat_penalty=1.1,
+            stop=["</s>"],
+        )
+
+        text_output = output["choices"][0]["text"]
+        return text_output
